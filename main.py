1395718de3bd93bfdb464f11d6405c0599792af6
Anthony Galczak <wgalczak@gmail.com>
2018-08-24 14:52:34 -0600

    Main + beginner csv file.

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 14:38:29 2018
+
+@author: anthony
+"""
+
+import pandas
+
+
+def main():
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
